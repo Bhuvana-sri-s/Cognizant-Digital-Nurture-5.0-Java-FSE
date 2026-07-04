2e379d9902d357d3f4682d374a8d77f2da747384
Bhuvana-sri-s <144764687+Bhuvana-sri-s@users.noreply.github.com>
2026-07-04 21:25:19 +0530

    Create CalculatorTest.java

---
+++ JUnit_BasicTestingExercise/Exercise4/src/main/java/com/example/CalculatorTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    // Setup method
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup completed.");
+    }
+
+    // Teardown method
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown completed.");
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
